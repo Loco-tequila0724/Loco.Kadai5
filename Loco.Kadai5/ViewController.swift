0ffab9cad093a07f3ed3341c2f2d26f69181accc
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-06-19 14:43:19 +0900

    first commit

--- Loco.Kadai5/ViewController.swift
+++ Loco.Kadai5/ViewController.swift
@@ -1,19 +1,32 @@
-//
-//  ViewController.swift
-//  Loco.Kadai5
-//
-//  Created by 日高隼人 on 2021/06/17.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak private var firstTextField: UITextField!
+    @IBOutlet weak private var secondTextField: UITextField!
+    @IBOutlet weak private var calculateNumberLabel: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+// 計算式　割り算
+    private func calculateDivision() {
+        let firstNumber = Double(firstTextField.text!) ?? 0
+        let secondNumber = Double(secondTextField.text!) ?? 0
+
+        calculateNumberLabel.text = String(firstNumber / secondNumber)
+    }
+// アラート機能
+    private func callWarningAlert(message: String) {
+        let alertController: UIAlertController = UIAlertController(title: "課題５", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+        present(alertController, animated: true)
     }
 
-
+    @IBAction private func calculateButton(_ sender: Any) {
+        if secondTextField.text!.isEmpty {
+            callWarningAlert(message: "割る数を入力してください🐥")
+        } else if secondTextField.text! == "0" {
+            callWarningAlert(message: "割る時には、０を入力しないで下さい🐥")
+        } else {
+            calculateDivision()
+        }
+    }
 }
-
