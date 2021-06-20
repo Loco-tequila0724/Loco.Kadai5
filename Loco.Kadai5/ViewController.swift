0f713e0f899db9e09ec1d858d9990bdab312e313
akio0911 <akio0911@gmail.com>
2021-06-21 06:25:32 +0900

    feedback

--- Loco.Kadai5/ViewController.swift
+++ Loco.Kadai5/ViewController.swift
@@ -5,28 +5,30 @@ class ViewController: UIViewController {
     @IBOutlet weak private var secondTextField: UITextField!
     @IBOutlet weak private var calculateNumberLabel: UILabel!
 
-// 計算式　割り算
-    private func calculateDivision() {
-        let firstNumber = Double(firstTextField.text!) ?? 0
-        let secondNumber = Double(secondTextField.text!) ?? 0
-
-        calculateNumberLabel.text = String(firstNumber / secondNumber)
-    }
-// アラート機能
-    private func callWarningAlert(message: String) {
-        let alertController: UIAlertController = UIAlertController(title: "課題５", message: message, preferredStyle: .alert)
+    // アラート機能
+    private func presentWarningAlert(message: String) {
+        let alertController = UIAlertController(title: "課題５", message: message, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
 
         present(alertController, animated: true)
     }
 
     @IBAction private func calculateButton(_ sender: Any) {
-        if secondTextField.text!.isEmpty {
-            callWarningAlert(message: "割る数を入力してください🐥")
-        } else if secondTextField.text! == "0" {
-            callWarningAlert(message: "割る時には、０を入力しないで下さい🐥")
-        } else {
-            calculateDivision()
+        guard let firstNumber = Double(firstTextField.text!) else {
+            presentWarningAlert(message: "割られる数を入力してください🐥")
+            return
         }
+
+        guard let secondNumber = Double(secondTextField.text!) else {
+            presentWarningAlert(message: "割る数を入力してください🐥")
+            return
+        }
+
+        guard secondNumber != 0 else {
+            presentWarningAlert(message: "割る時には、０を入力しないで下さい🐥")
+            return
+        }
+
+        calculateNumberLabel.text = String(firstNumber / secondNumber)
     }
 }
